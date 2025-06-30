6862bd4f2e0aaea565e1d61df35be7132ccdbaf7
Felipe Yunoki <f.kenjiyunoki@gmail.com>
2025-06-30 08:44:07 -0300

    feat: add cadastrar_produto function

--- estoque.py
+++ estoque.py
@@ -12,6 +12,20 @@ def menu():
     print(Fore.YELLOW + "5. Excluir Produto")
     print(Fore.YELLOW + "6. Sair")
 
+def cadastrar_produto():
+    nome = input(Fore.CYAN + "Digite o nome do produto: ")
+    preco = float(input(Fore.CYAN + "Digite o preço do produto: "))
+    quantidade = int(input(Fore.CYAN + "Digite a quantidade do produto: "))
+    
+    produto = {
+        'nome': nome,
+        'preco': preco,
+        'quantidade': quantidade
+    }
+    
+    produtos.append(produto)
+    print(Fore.GREEN + "Produto cadastrado com sucesso!")
+    
 menu()
 
 #py -m pip install colorama
